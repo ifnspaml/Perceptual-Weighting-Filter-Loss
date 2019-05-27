1d93c7fe39ca6d3feec676c3c4c6ed862bc47320
ZiyueZhao <38912731+ZiyueZhao@users.noreply.github.com>
2019-05-27 16:30:25 +0200

    Update GitHub_all_test_mask_dnn_weight_filter.py

--- GitHub_all_test_mask_dnn_weight_filter.py
+++ GitHub_all_test_mask_dnn_weight_filter.py
@@ -149,7 +149,7 @@ for k_snr in range(0, len(SNR_situ_array)):
     learning_rate = 5e-4
     adam_wn = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     model.compile(optimizer=adam_wn, loss='mean_squared_error', metrics=['accuracy'])
-    model.load_weights("./training results/mask_dnn_weight_filter_" + filter_type_str + "_" + noi_situ_model_str+ "_weights_negA.h5")
+    model.load_weights("./training results/mask_dnn_weight_filter_" + filter_type_str + "_" + noi_situ_model_str+ "_weights.h5")
 
     #####################################################################################
     # 2. load data
